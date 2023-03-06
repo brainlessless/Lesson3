2b6e92bbbd61808fe410d2ccf9841a3f692dbe18
Vitaly <109906595+brainlessless@users.noreply.github.com>
2023-03-06 16:57:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,123 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1 задание
+
+        //   int a = 9;
+        //   if (a == 10) {
+        //       System.out.println("Верно");
+        //   }
+        //   if (a < 10) {
+        //       System.out.println("Неверно");
+        //   }
+
+        // 2 задание
+
+        //   int min = 72;
+        //   if (min >= 0 && min <= 14) {
+        //       System.out.println("Первая четверть");
+        //   }
+        //   if (min >= 15 && min <= 29) {
+        //       System.out.println("Вторая четверть");
+        //   }
+        //   if (min >= 30 && min <= 44) {
+        //       System.out.println("Третья четверть");
+        //   }
+        //   if (min >= 45 && min <= 59) {
+        //       System.out.println("Четвертая четверть");
+        //   }
+        //   if (min > 60) {
+        //       System.out.println("Неправильное число");
+        //   }
+
+
+        // 3 задание
+
+        //  int a = -3;
+        //  if (a>0) {
+        //      System.out.println("Верно");
+        //  }
+        //  else {
+        //      System.out.println("Неверно");
+        //  }
+
+        // 4 задание
+
+        //  int a = -3;
+        //  if (a<0) {
+        //      System.out.println("Верно");
+        //  }
+        //  else {
+        //      System.out.println("Неверно");
+        //  }
+
+        // 5 задание
+
+        // int a = -1;
+        // if (a >= 0) {
+        //     System.out.println("Верно");
+        // } else {
+        //     System.out.println("Неверно");
+        // }
+
+        // 6 задание
+
+        //    int a = -3;
+        //    if (a <= 0) {
+        //        System.out.println("Верно");
+        //    } else {
+        //        System.out.println("Неверно");
+        //    }
+
+        // 7 задание
+
+        //   int a = 0;
+        //   if (a != 0) {
+        //       System.out.println("Верно");
+        //   } else {
+        //       System.out.println("Неверно");
+        //   }
+
+        // 10 задание
+
+        //  int a = 50;
+        //  int b = 10;
+        //  int c = 40;
+
+        //  if (a>b)
+        //  if (a>c){
+        //      System.out.println("max" + a);
+        //  }
+        //  else {
+        //      System.out.println("max" + c);
+        //  }
+        //  if (b>c)
+        //  if (b>a){
+        //      System.out.println("max" + b);
+        //  }
+        //  else {
+        //      System.out.println("max" + c);
+        //  }
+
+        // 11 задание
+
+
+        //   int a = -10;
+        //   if (a>0) {
+        //       System.out.println("Число положительное");
+        //   }
+        //   if (a<0) {
+        //       System.out.println("Число отрицательное");
+        //   }
+
+        // 12 задание
+
+        int a = 41;
+
+        if (a%2==0) {
+            System.out.println("Четное число");
+        }
+        if (a%2!=0) {
+            System.out.println("Нечетное число");
+        }
+    }
+}
